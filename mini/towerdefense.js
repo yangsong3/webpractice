6e967b01ca48847f7adf7dfca60feb93dd607ef4
DESKTOP-98O1SRU\301-09 <qkghl911@gmail.com>
2025-02-13 15:50:25 +0900

    backup

--- mini/towerdefense.js
+++ mini/towerdefense.js
@@ -1,4 +1,36 @@
-// 맵 기본 배경 타일 깔기
+// html 화면 전환
+function startPage() {
+  document.getElementById("startPage").style.display = "none"; // 게임 시작 후 첫 페이지 숨기기
+  document.getElementById("gameframe").style.display = "block"; // 게임 화면 표시하기
+}
+
+function difficultyPage() {
+  document.getElementById("startPage").style.display = "none"; // 첫 번째 페이지 숨기기
+  document.getElementById("setframe").style.display = "block"; // 난이도 설정 페이지 표시하기
+}
+
+function backPage() {
+  document.getElementById("startPage").style.display = "block"; // 첫 번째 페이지 다시 표시
+  document.getElementById("setframe").style.display = "none"; // 난이도 설정 페이지 숨기기
+}
+
+function selectDifficulty(level) {
+  console.log("선택한 난이도:", level);
+  document.getElementById("setframe").style.display = "none"; // 게임 시작 후 첫 페이지 숨기기
+  document.getElementById("gameframe").style.display = "block"; // 게임 화면 표시하기
+}
+
+function showImage(imageUrl, name, damage, attackSpeed, cost) {
+  const itemInfo = document.getElementById("itemInfo");
+  itemInfo.innerHTML = `
+      <img src="${imageUrl}" width="140" height="60">
+      <p>이름: ${name}</p>
+      <p>데미지: ${damage}</p>
+      <p>공격속도 및 범위: ${attackSpeed}</p>
+      <p>필요골드: ${cost}</p>
+  `;
+}
+
 class Tile {
   static rows = 6;
   static cols = 10;
@@ -131,7 +163,7 @@ class Tower {
     bctx.closePath();
   }
 
-  static updateTowers(){
+  static updateTowers() {
     Tower.towers.forEach((tower) => {
       tower.findTarget();
       if (tower.target) {
@@ -144,7 +176,18 @@ class Tower {
 class Monster {
   static size = 80;
   static path = [11, 41, 45, 15, 18, 38, 39];
-  static monstersbywave = { 1: 5, 2: 10, 3: 15, 4: 20 };
+  static monstersbywave = {
+    1: 5,
+    2: 10,
+    3: 15,
+    4: 20,
+    5: 25,
+    6: 30,
+    7: 35,
+    8: 40,
+    9: 45,
+    10: 50,
+  };
   static monsters = [];
   static imgSrc =
     "https://png.pngtree.com/png-clipart/20190618/original/pngtree-anime-character-campus-game-character-anime-comic-hand-drawn-png-image_3923580.jpg";
@@ -209,8 +252,15 @@ class Monster {
         Monster.size,
         Monster.size
       );
+
+      // 몬스터 체력바
       bctx.fillStyle = "red";
-      bctx.fillRect(this.x - 20, this.y -50, 40 * (this.hp / 20), 5);
+      bctx.fillRect(
+        this.x - Monster.size / 2,
+        this.y - 50,
+        (Tile.tileSize / 2) * (this.hp / 20),
+        5
+      );
     }
   }
 
@@ -258,9 +308,8 @@ class Monster {
     }
 
     if (Monster.monsters.length === 0) {
-      console.log("모든 몬스터가 사라짐. 애니메이션 중지!");
-      clearInterval(gameFrameInterval);
-      gameFrameLoop = false;
+      // clearInterval(gameFrameInterval);
+      // gameFrameLoop = false;
       Status.waveUpdate((Status.wave += 1));
     }
   }
@@ -289,10 +338,10 @@ class Projectile {
     }
 
     this.x += (dx / distance) * this.speed;
-    this.y += (dy /distance) * this.speed;
+    this.y += (dy / distance) * this.speed;
   }
 
-  hitTarget(){
+  hitTarget() {
     if (this.target) {
       this.target.hp -= 1;
 
@@ -300,17 +349,16 @@ class Projectile {
         this.removeMonster(this.target);
       }
     }
-    Projectile.projectiles.splice(Projectile.projectiles.indexOf(this),1);
+    Projectile.projectiles.splice(Projectile.projectiles.indexOf(this), 1);
   }
 
-  removeMonster(monster){
+  removeMonster(monster) {
     let index = Monster.monsters.indexOf(monster);
-    if ( index !== -1){
+    if (index !== -1) {
       Monster.monsters.splice(index, 1);
-      Status.goldUpdate(Status.gold += 10);
+      Status.goldUpdate((Status.gold += 10));
     }
   }
-
 
   draw(bctx) {
     bctx.fillStyle = "blue";
@@ -369,6 +417,19 @@ class Status {
     life.innerText = `life : ${Status.life}`;
     gold.innerText = `Gold : ${Status.gold}`;
   }
+
+  static checkLife() {
+    if (Status.life <= 0) {
+      clearInterval(drawFrame);
+      clearInterval(gameFrameInterval);
+      document.getElementsByClassName("container")[0].style.display = "none";
+      let endpage = document.createElement("div");
+      endpage.style =
+        "text-align: center; background-color: black; color: white; vertical-align: middle;";
+      endpage.innerHTML = "<h1>Game Over</h1><br>";
+      document.body.appendChild(endpage);
+    }
+  }
 }
 
 // 캔버스
@@ -387,79 +448,115 @@ function statusDisplay() {
 // 우측 타워 아이템 클릭 이벤트
 const towerClass = document.getElementsByClassName("item");
 let hasItem = false;
+let selectedImgSrc = ""; // 선택한 타워 이미지 저장
 
 // 우측 타워 아이템 클래스 전체를 돌며 이벤트 설정
 for (let i = 0; i < towerClass.length; i++) {
-  towerClass[i].addEventListener("click", function(e) {
-    let img = new Image();
+  towerClass[i].addEventListener("click", function (e) {
+    if (hasItem) return; // 구입된 상태에서 중복 구매되는거 방지
+
     if (Array.from(document.querySelectorAll(".itemimg")).includes(e.target)) {
-      console.log("sucess");
-      img.src = e.target.src;
       if (Status.gold >= e.target.alt) {
+        selectedImgSrc = e.target.src; // 선택한 이미지 저장
         hasItem = true;
         Status.goldUpdate((Status.gold -= e.target.alt));
         console.log("구매성공, 남은 골드 :", Status.gold);
 
-        bcvs.addEventListener("click", (e) => {
-          if (hasItem) {
-            for (let i = 0; i < Tile.tiles.length; i++) {
-              if (
-                Tile.tiles[i].midX + Tile.tileSize / 2 > e.offsetX &&
-                Tile.tiles[i].midY + Tile.tileSize / 2 > e.offsetY &&
-                Tile.tiles[i].midX - Tile.tileSize / 2 < e.offsetX &&
-                Tile.tiles[i].midY - Tile.tileSize / 2 < e.offsetY
-              ) {
-                if (Tile.tiles[i].isInstallable) {
-                  let tower = new Tower(
-                    img,
-                    Tile.tiles[i].midX,
-                    Tile.tiles[i].midY,
-                    5
-                  );
-                  tower.draw(bctx);
-                  Tower.towers.push(tower);
-                  Tile.tiles[i].isInstallable = false;
-                  console.log(Tile.tiles[i], Tower.towers);
-                  hasItem = false;
-                } else {
-                  console.log("타워 설치 불가");
+        bcvs.addEventListener(
+          "click",
+          function placeTower(event) {
+            if (hasItem) {
+              for (let i = 0; i < Tile.tiles.length; i++) {
+                if (
+                  Tile.tiles[i].midX + Tile.tileSize / 2 > event.offsetX &&
+                  Tile.tiles[i].midY + Tile.tileSize / 2 > event.offsetY &&
+                  Tile.tiles[i].midX - Tile.tileSize / 2 < event.offsetX &&
+                  Tile.tiles[i].midY - Tile.tileSize / 2 < event.offsetY
+                ) {
+                  if (Tile.tiles[i].isInstallable) {
+                    let img = new Image();
+                    img.width = Tower.towerSize;
+                    img.height = Tower.towerSize;
+                    img.src = selectedImgSrc; // 현재 선택된 이미지 적용
+
+                    let tower = new Tower(
+                      img,
+                      Tile.tiles[i].midX,
+                      Tile.tiles[i].midY,
+                      5
+                    );
+                    tower.draw(bctx);
+                    Tower.towers.push(tower);
+                    Tile.tiles[i].isInstallable = false;
+                    hasItem = false;
+
+                    bcvs.removeEventListener("click", placeTower);
+                    console.log("타워 설치");
+                    return;
+                  } else {
+                    console.log("타워 설치 불가");
+                  }
                 }
               }
             }
-          }
-        });
+          },
+          { once: true }
+        );
       } else {
         console.log("골드 부족, 보유 골드 :", Status.gold);
       }
     } else {
       return;
     }
-    img.style.position = "absolute";
-    img.width = Tower.towerSize;
-    img.height = Tower.towerSize;
   });
+}
+
+// 기본 상태 -> 맵, 타워 그리기
+function drawState() {
+  Tile.drawMap(bctx);
+  Tower.towers.forEach((tower) => tower.draw(bctx));
+  // console.log("drawState");
+
+  // 캔버스 픽셀 데이터 확인
+  // let pixels = bctx.getImageData(0, 0, canvas.width, canvas.height).data;
+  // console.log("drawState 후 픽셀 데이터:", pixels.slice(0, 100));
 }
 
 // 게임진행 프레임 그리기
 function drawFrame() {
-  Tile.drawMap(bctx);
-  Tower.towers.forEach((tower) => tower.draw(bctx));
+  drawState();
   Tower.updateTowers();
   Projectile.updateProjectiles(bctx);
   Monster.monsterFrame(bctx);
   Status.display();
-  console.log("drawFrame");
+  // console.log("drawFrame");
+
+  if (Monster.monsters.length === 0) {
+    stopGameFrameLoop();
+  }
 }
 
-// 메인 화면
+function stopGameFrameLoop() {
+  if (gameFrameLoop) {
+    clearInterval(gameFrameInterval);
+    gameFrameLoop = false;
+    drawState();
+
+    console.log("stopGameFrameLoop");
+  }
+}
+
+// 메인 화면 초기화
 function init() {
   Tile.setting();
   statusDisplay();
-  Tile.drawMap(bctx);
-  Tower.towers.forEach((tower) => tower.draw(bctx));
+  drawState();
+  console.log("init");
 }
 
-// 캔버스 동적 물체 루프 시작 여부
+init();
+
+// 캔버스 루프 작동 여부
 let gameFrameLoop = false;
 
 // 인터벌 설정
@@ -477,9 +574,9 @@ startBtn.addEventListener("click", () => {
   );
   if (!gameFrameLoop) {
     gameFrameLoop = true;
-    gameFrameInterval = setInterval(drawFrame, 1000 / 244);
-    console.log("setInterval");
+    gameFrameInterval = setInterval(() => {
+      Status.checkLife();
+      drawFrame();
+    }, 1000 / 244);
   }
 });
-
-init();
